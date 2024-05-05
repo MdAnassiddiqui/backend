f67a0881ccbe1cac29d058cc0131d91e9bb64caf
MdAnassiddiqui <anas8933ald9@gmail.com>
2024-05-05 11:01:01 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3001' 
+}));
+
+let data = {};
+let addCount = 0; 
+let updateCount = 0; 
+
+// API to add new data
+app.post('/add', (req, res) => {
+  data = req.body; 
+  addCount++; 
+  res.status(201).send('Data added successfully');
+});
+
+// API to update existing data
+app.patch('/update', (req, res) => {
+  data = { ...data, ...req.body }; 
+  updateCount++; 
+  res.send('Data updated successfully');
+});
+
+
+app.get('/count', (req, res) => {
+  res.json({ addCount, updateCount });
+});
+
+
+app.get('/addCount', (req, res) => {
+  res.json({ addCount });
+});
+
+app.get('/updateCount', (req, res) => {
+  res.json({ updateCount });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
