0a2e907f9e7b6dd7bac6b1e43e20027f0066aad7
MdAnassiddiqui <anas8933ald9@gmail.com>
2024-05-05 18:20:08 -0700

    cvf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: '*'
+    origin: 'https://addand-update-vakj.vercel.app'
 }));
 
 let items = []; 
